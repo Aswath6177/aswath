e17b79cbd1a3e16ee29aa54d41db1184a5c89cac
palaji <123574896+palaji@users.noreply.github.com>
2024-07-08 13:19:14 +0530

    gcchgj

---
+++ src/demoproject/addtwonumbers.java
@@ -0,0 +1,17 @@
+package demoproject;
+
+public class addtwonumbers {
+
+	public static void main(String[] args) {
+		int number1 = 5;
+        int number2 = 3;
+        
+        // Add the numbers
+        int sum = number1 + number2;
+        
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+
+	}
+
+}
